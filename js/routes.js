287438bff7c7776af0167b34b401cd7b498d1ff0
Paul <52529860@qq.com>
2015-09-09 10:19:24 +0800

    修改url

--- js/routes.js
+++ js/routes.js
@@ -5,7 +5,7 @@
  * =====================================================
  */
 (function(w) {
-	var domain = 'http://192.168.0.102:3000/'; //production
+	var domain = 'http://115.28.59.227:3333/'; //production
 	w.Routes = {
 		domain: domain,
 		urls: {
